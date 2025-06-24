c845ff6a21237dde2e41bfd85783d5fb42981adc
j2-err <javier.alvarez.g@usach.cl>
2025-06-24 11:50:15 -0400

    3333333

---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="gdrive_conector",
+    version="0.1.0",
+    description="Conector a Google Drive y librerias para pasar a dataframes",
+    author="Tu Nombre",
+    url="https://github.com/j2-err/gdrive-conector",
+    packages=find_packages(),
+    install_requires=[
+        "google-api-python-client",
+        "google-auth-httplib2",
+        "google-auth-oauthlib",
+        "pandas",
+        "openpyxl",
+            # Para leer archivos Excel
+    ],
+)
