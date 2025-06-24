cac85f3253f46a2558ea6d410b057d1d2847560e
j2-err <javier.alvarez.g@usach.cl>
2025-06-24 12:51:30 -0400

    22

--- gdrive2pandas/gdrive2pandas.py
+++ gdrive2pandas/gdrive2pandas.py
@@ -1,8 +1,12 @@
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.http import MediaIoBaseDownload
+import io
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
 from pathlib import Path
+import pandas as pd
+
 
 # Alcances de permisos (puedes personalizar)
 SCOPES = ['https://www.googleapis.com/auth/drive']
@@ -37,3 +41,50 @@ def conectar_drive():
     service = build('drive', 'v3', credentials=creds)
 
     return service
+
+
+
+
+
+
+drive_service = conectar_drive()
+
+
+def listar_archivos(carpeta_id:str):
+    archivos = []
+    page_token = None
+
+    while True:
+        response = drive_service.files().list(
+            q=f"'{carpeta_id}' in parents and trashed = false",
+            fields="nextPageToken, files(id, name, mimeType)",
+            pageSize=1000,
+            pageToken=page_token
+        ).execute()
+
+        archivos.extend(response.get('files', []))
+        page_token = response.get('nextPageToken', None)
+
+        if page_token is None:
+            break
+
+    return archivos
+
+def drive_a_dataframe(archivo_id: str):
+    request = drive_service.files().get_media(fileId=archivo_id)
+    file = io.BytesIO()
+    downloader = MediaIoBaseDownload(file, request)
+
+    done = False
+    while not done:
+        _, done = downloader.next_chunk()
+
+    file.seek(0)
+
+    # Leer directo a pandas
+    df = pd.read_excel(file)
+    print('Archivo leído correctamente')
+    return df
+
+
+#print(f"Archivos encontrados en la carpeta: {archivos}")
